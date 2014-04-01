18260df80b5be84993745bdaebb5abd566fec9a8
superwuxin <870757543@qq.com>
2014-04-01 22:08:19 +0800

    JTA测试代码

---
+++ src/main/java/org/moon/tomee/jta/impl/UserCMPDaoImpl.java
@@ -0,0 +1,43 @@
+package org.moon.tomee.jta.impl;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.moon.tomee.jpa.persistence.User;
+import org.moon.tomee.jta.UserDao;
+
+@Stateless
+@TransactionManagement(TransactionManagementType.CONTAINER)
+public class UserCMPDaoImpl implements UserDao {
+
+	@PersistenceContext(unitName = "hibernate-moon")
+	private EntityManager em;
+	@EJB(beanName = "UserBMPDaoImpl")
+	private UserDao userDao;
+	
+	@Override
+	public void saveUser(User user) {
+		em.persist(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		em.remove(em.find(User.class, user.getId()));
+	}
+
+	@Override
+	public void saveWithOtherDao(User user1, User user2) {
+		em.persist(user1);
+		userDao.saveUser(user2);
+	}
+
+	@Override
+	public long count() {
+		return (long) em.createQuery("select count(o) from User o").getSingleResult();
+	}
+
+}
