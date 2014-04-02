da30919beb75fee11f8c7e4954dfa4e34fdb62a9
superwuxin <870757543@qq.com>
2014-04-02 09:00:57 +0800

    Mail.java conflict

--- src/main/java/org/moon/tomee/mail/Mail.java
+++ src/main/java/org/moon/tomee/mail/Mail.java
@@ -1,21 +1,24 @@
 package org.moon.tomee.mail;
 
-public class Mail {
-	
-	private String fromAddress;
-	private String toAddress;
-	private Object content;
-	private String subject;
-	private String[] attachFileNames;
+public abstract class Mail {
+
+	protected String fromAddress;
+	protected String[] toAddresses;
+	protected String[] ccAddresses;
+	protected String[] bccAddresses;
+	protected String subject;
+	protected Object content;
+	protected String[] attachFileNames;
+	protected MailTemplet mailTemplet;
 
 	public Mail() {
 	}
 
-	public Mail(String fromAddress, String toAddress, Object content, String subject) {
+	public Mail(String fromAddress, String[] toAddresses, String subject, Object content) {
 		this.fromAddress = fromAddress;
-		this.toAddress = toAddress;
-		this.content = content;
+		this.toAddresses = toAddresses;
 		this.subject = subject;
+		this.content = content;
 	}
 
 	public String getFromAddress() {
@@ -26,20 +29,28 @@ public class Mail {
 		this.fromAddress = fromAddress;
 	}
 
-	public String getToAddress() {
-		return toAddress;
+	public String[] getToAddresses() {
+		return toAddresses;
 	}
 
-	public void setToAddress(String toAddress) {
-		this.toAddress = toAddress;
+	public void setToAddresses(String[] toAddresses) {
+		this.toAddresses = toAddresses;
 	}
 
-	public Object getContent() {
-		return content;
+	public String[] getCcAddresses() {
+		return ccAddresses;
 	}
 
-	public void setContent(Object content) {
-		this.content = content;
+	public void setCcAddresses(String[] ccAddresses) {
+		this.ccAddresses = ccAddresses;
+	}
+
+	public String[] getBccAddresses() {
+		return bccAddresses;
+	}
+
+	public void setBccAddresses(String[] bccAddresses) {
+		this.bccAddresses = bccAddresses;
 	}
 
 	public String getSubject() {
@@ -50,6 +61,22 @@ public class Mail {
 		this.subject = subject;
 	}
 
+	public Object getContent() {
+		return content;
+	}
+
+	public void setContent(Object content) {
+		this.content = content;
+	}
+
+	public MailTemplet getMailTemplet() {
+		return mailTemplet;
+	}
+
+	public void setMailTemplet(MailTemplet mailTemplet) {
+		this.mailTemplet = mailTemplet;
+	}
+
 	public String[] getAttachFileNames() {
 		return attachFileNames;
 	}
@@ -57,4 +84,5 @@ public class Mail {
 	public void setAttachFileNames(String[] attachFileNames) {
 		this.attachFileNames = attachFileNames;
 	}
+
 }
