c9fe8568ce854b654741789d061dd9d110a7e77d
superwuxin <870757543@qq.com>
2014-04-02 22:37:45 +0800

    JPA 测试类修改

--- src/test/java/org/moon/tomee/jpa/JPAORMTest.java
+++ src/test/java/org/moon/tomee/jpa/JPAORMTest.java
@@ -1,7 +1,5 @@
 package org.moon.tomee.jpa;
 
-import static org.junit.Assert.*;
-
 import java.util.Properties;
 
 import javax.annotation.Resource;
@@ -10,52 +8,45 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 
-import org.junit.AfterClass;
+import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
-import org.moon.tomee.jpa.persistence.Passport;
-import org.moon.tomee.jpa.persistence.User;
 
 public class JPAORMTest {
 
-	private static EJBContainer container;
+	private EJBContainer container;
 	@PersistenceContext(unitName = "hibernate-moon")
 	private EntityManager em;
 	@Resource
 	private UserTransaction ux;
 	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 		Properties props = new Properties();
 		props.put("openejb.conf.file", "src/main/resources/conf/openejb.xml");
 		container = EJBContainer.createEJBContainer(props);
-	}
-	
-	@Before
-	public void setUp() throws Exception{
 		container.getContext().bind("inject", this);
 	}
 	
 	@Test
+	@Ignore
 	public void testOneToOne() throws Exception {
-		ux.begin();
-		User user = new User("AAA");
-		Passport passport = new Passport("China");
-		passport.setUser(user);
-		user.setPassport(passport);
-		em.persist(user);
-		ux.commit();
-		User user2 = null;
-		Passport passport2 = null;
-		assertNotEquals(null, (user2 = (User)em.find(User.class, user.getUserId())));
-		assertNotEquals(null, (passport2 = user2.getPassport()));
-		assertEquals(user.getUsername(), user2.getUsername());
-		assertEquals(passport.getCountry(), passport2.getCountry());
+		
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+	@Test
+	public void testOneToMany() throws Exception {
+
+	}
+	
+	@Test
+	public void testManyToMany() throws Exception {
+		
+	}
+	
+	@After
+	public void tearDown() throws Exception {
 		container.close();
 	}
 
