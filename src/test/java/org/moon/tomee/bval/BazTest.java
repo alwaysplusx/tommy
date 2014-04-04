f9b3a330ad1a8f00cc0553623494e167f77a627f
superwuxin <870757543@qq.com>
2014-04-04 11:55:31 +0800

    BVal测试部分代码

---
+++ src/test/java/org/moon/tomee/bval/BazTest.java
@@ -0,0 +1,36 @@
+package org.moon.tomee.bval;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BazTest {
+
+	private Validator validator;
+
+	@Before
+	public void setUp() throws Exception {
+		validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
+
+	@Test
+	public void testBar() {
+		Baz baz = new Baz();
+		baz.setText("this is automata");
+		Set<ConstraintViolation<Baz>> cvs = validator.validate(baz);
+		for(ConstraintViolation<?> cv : cvs){
+			System.out.println(cv.getMessage());
+		}
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
---
+++ src/test/java/org/moon/tomee/bval/CitizenTest.java
@@ -0,0 +1,35 @@
+package org.moon.tomee.bval;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CitizenTest {
+
+	private Validator validator;
+
+	@Before
+	public void setUp() throws Exception {
+		validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
+
+	@Test
+	public void testCitizen() {
+		Citizen citizen = new Citizen();
+		//citizen.setIdCard("111111111111111");
+		Set<ConstraintViolation<Citizen>> cvs = validator.validate(citizen);
+		assertEquals("violation is not empty", false, cvs.isEmpty());
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+}
